b5c62ad6d9b9df8a5bf8a1c1cd81ae8cd4611201
Nirmal <nambasana033@gmail.com>
2024-02-10 16:53:39 +0530

    sidebar working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
 const sidebar = document.getElementById("sidebar");
+const navbar = document.getElementById("navbar");
+const mainContainer = document.getElementById("main-container");
 
-// sidebar.style.position = "absolute";
+let isSidebaropen = false;
+
+sidebar.style.left = "-21rem";
+mainContainer.style.border = "1px solid red"
+const openSidebar = () => {
+    if (!isSidebaropen) {
+        sidebar.style.left = 0
+        mainContainer.style.marginLeft = "21rem"
+        isSidebaropen = true
+    } else {
+        sidebar.style.left = "-21rem"
+        mainContainer.style.marginLeft = "0"
+        isSidebaropen = false
+    }
+}
