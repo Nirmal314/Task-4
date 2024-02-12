19c9c2a669f3c146039b20fdab13265be0768b32
Nirmal Ambasana <sit441@sit.local>
2024-02-12 09:36:26 +0530

    final main commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 const sidebar = document.getElementById("sidebar");
+const nav = document.getElementsById("nav");
 
-// sidebar.style.position = "absolute";
+sidebar.style.position = "absolute";
+sidebar.style.left = "-21rem";
+sidebar.style.transition = "all 500ms ease";
+
+// const openSidebar = () => {
+//     sidebar.style.left = "0";
+//     nav.style.marginLeft = "21rem";
+// }
