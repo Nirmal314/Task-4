52325789c915ed6b7b71a2874eea12dec9e167b5
Nirmal Ambasana <sit441@sit.local>
2024-02-12 18:51:07 +0530

    commit #2

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,40 @@ const mainContainer = document.getElementById("main-container");
 
 let isSidebaropen = false;
 
-sidebar.style.left = "-21rem";
-mainContainer.style.border = "1px solid red"
+// sidebar.style.left = "-21rem";
+// sidebar.style.display = "none";
+mainContainer.style.marginLeft = "21rem"
+// const openSidebar = () => {
+//     if (!isSidebaropen) {
+//         sidebar.style.left = 0
+//         mainContainer.style.marginLeft = "21rem"
+//         isSidebaropen = true
+//     } else {
+//         sidebar.style.left = "-21rem"
+//         mainContainer.style.marginLeft = "0"
+//         isSidebaropen = false
+//     }
+// }
+
+const sidebarItems = document.querySelectorAll(".fs-sidebar");
+const sidebarTexts = document.querySelectorAll(".sidebar-nav-text");
+
 const openSidebar = () => {
     if (!isSidebaropen) {
-        sidebar.style.left = 0
-        mainContainer.style.marginLeft = "21rem"
+        sidebarItems.forEach((sidebarItem) => {
+            sidebarItem.style.flexDirection = "row"
+        });
+        sidebarTexts.forEach((sidebarText) => {
+            sidebarText.style.fontSize = ".65rem"
+        });
         isSidebaropen = true
     } else {
-        sidebar.style.left = "-21rem"
-        mainContainer.style.marginLeft = "0"
+        sidebarItems.forEach((sidebarItem) => {
+            sidebarItem.style.flexDirection = "column"
+        });
+        sidebarTexts.forEach((sidebarText) => {
+            sidebarText.style.fontSize = "18px"
+        });
         isSidebaropen = false
     }
 }
