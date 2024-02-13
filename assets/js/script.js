8c9c4f9498ba865df558f9cfcf1fbf6c07cafe82
Nirmal Ambasana <sit441@sit.local>
2024-02-13 17:28:39 +0530

    v1

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,41 +3,129 @@ const navbar = document.getElementById("navbar");
 const mainContainer = document.getElementById("main-container");
 
 let isSidebaropen = false;
+let isMobileSidebaropen = false;
 
-// sidebar.style.left = "-21rem";
-// sidebar.style.display = "none";
-mainContainer.style.marginLeft = "21rem"
-// const openSidebar = () => {
-//     if (!isSidebaropen) {
-//         sidebar.style.left = 0
-//         mainContainer.style.marginLeft = "21rem"
-//         isSidebaropen = true
-//     } else {
-//         sidebar.style.left = "-21rem"
-//         mainContainer.style.marginLeft = "0"
-//         isSidebaropen = false
-//     }
-// }
+mainContainer.style.marginLeft = "92px"
 
 const sidebarItems = document.querySelectorAll(".fs-sidebar");
 const sidebarTexts = document.querySelectorAll(".sidebar-nav-text");
+const sidebarIcons = document.querySelectorAll(".sidebar-icons");
+const sidebarUl = document.querySelector(".sidebar-ul");
+const sidebarLi = document.querySelectorAll(".sidebar-li");
+const sidebarA = document.querySelectorAll(".sidebar-a");
+const sidebarCard = document.getElementById("sidebar-card");
+
+sidebarTexts.forEach((sidebarText) => {
+    sidebarText.style.display = "none"
+})
+sidebarUl.classList.add("p-3");
+sidebarA.forEach((a) => {
+    a.classList.add("justify-content-center");
+})
+sidebarCard.style.display = "none";
+sidebar.classList.add("position-fixed");
+
+window.onload = () => {
+    const setSidebarHeight = () => {
+        var mainContainerHeight = mainContainer.scrollHeight;
+        sidebar.style.height = mainContainerHeight + 'px';
+    }
+
+    setSidebarHeight();
+
+    window.addEventListener('resize', setSidebarHeight);
+};
 
 const openSidebar = () => {
     if (!isSidebaropen) {
-        sidebarItems.forEach((sidebarItem) => {
-            sidebarItem.style.flexDirection = "row"
-        });
         sidebarTexts.forEach((sidebarText) => {
-            sidebarText.style.fontSize = ".65rem"
-        });
+            sidebarText.style.display = "block";
+        })
+        sidebarIcons.forEach((sidebarIcon) => {
+            sidebarIcon.style.marginRight = "20px"
+        })
+        sidebarUl.classList.add("p-5");
+        sidebarUl.classList.remove("p-3");
+        sidebarA.forEach((a) => {
+            a.classList.remove("justify-content-center");
+        })
+        sidebarCard.style.display = "block";
+        mainContainer.style.marginLeft = "334px"
+
+        sidebar.classList.remove("position-fixed");
+        sidebar.classList.add("position-absolute");
+
         isSidebaropen = true
     } else {
-        sidebarItems.forEach((sidebarItem) => {
-            sidebarItem.style.flexDirection = "column"
-        });
         sidebarTexts.forEach((sidebarText) => {
-            sidebarText.style.fontSize = "18px"
-        });
+            sidebarText.style.display = "none";
+        })
+        sidebarIcons.forEach((sidebarIcon) => {
+            sidebarIcon.style.marginRight = "0"
+        })
+        sidebarUl.classList.add("p-3");
+        sidebarUl.classList.remove("p-5");
+        sidebarA.forEach((a) => {
+            a.classList.add("justify-content-center");
+        })
+        sidebarCard.style.display = "none";
+        mainContainer.style.marginLeft = "92px"
+
+        sidebar.classList.remove("position-absolute");
+        sidebar.classList.add("position-fixed");
+
         isSidebaropen = false
     }
+    console.log(isSidebaropen)
 }
+
+const openSidebarMobile = () => {
+    if (!isMobileSidebaropen) {
+        sidebar.style.left = "0"
+        mainContainer.style.paddingLeft = "92px"
+        isMobileSidebaropen = true
+    } else {
+        sidebar.style.left = "-92px"
+        mainContainer.style.paddingLeft = "0"
+        isMobileSidebaropen = false
+    }
+}
+
+function attachClickHandler() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 576) {
+        // ! mobile view
+
+        // when resized, close both the sidebars
+        if (isSidebaropen) {
+            openSidebar()
+        }
+        if (isMobileSidebaropen) {
+            openSidebarMobile();
+        }
+
+        sidebar.style.left = "-92px"
+        mainContainer.style.marginLeft = "0"
+        document.getElementById("mobile-btn").onclick = openSidebarMobile;
+    } else {
+        // ! pc view
+
+        // when resized, close both the sidebars
+        if (isMobileSidebaropen) {
+            openSidebarMobile();
+        }
+        if (isSidebaropen) {
+            openSidebar()
+        }
+
+        sidebar.style.left = "0"
+        mainContainer.style.marginLeft = "92px"
+        document.getElementById("pc-btn").onclick = openSidebar;
+    }
+}
+
+// Initial call to attach appropriate click handler based on screen width
+attachClickHandler();
+
+// Event listener for window resize to update click handler
+window.addEventListener('resize', attachClickHandler);
